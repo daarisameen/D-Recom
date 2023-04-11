a6ae69e2170f75029a88235d1a65f08dad900360
Daaris Ameen <64462855+daarisameen@users.noreply.github.com>
2023-04-11 21:11:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -296,8 +296,28 @@ const texty2='[{"m_title": "Attack on Titan", "r": "9", "year": "2013"}, {"m_tit
            return false;
         }
       }
-      // heydown block
+      // keydown block
 
 
-     
-      // carosal
+      let mybutton = document.getElementById("myBtn");
+
+      // When the user scrolls down 20px from the top of the document, show the button
+      window.onscroll = function() {scrollFunction()};
+      
+      function scrollFunction() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+          mybutton.style.display = "block";
+        } else {
+          mybutton.style.display = "none";
+        }
+      }
+      
+      // When the user clicks on the button, scroll to the top of the document
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+
+      // scrollbtn
+
+
